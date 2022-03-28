7d237ad4c81b6593087586a99d1285147856a4df
LuizSouz404 <luiz.souz404@gmail.com>
2022-03-28 17:40:16 -0300

    feat: Table transaction componenet

---
+++ src/components/AdminComponents/TransactionTable/index.tsx
@@ -0,0 +1,41 @@
+import { MiniThumbsProducts } from '../../MiniThumbsProdcut';
+import { Container } from './styles';
+
+export const TransactionsTable = () => {
+  let transactions = [{id: "1"}, {id: "2"}];
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Cliente</th>
+            <th>Itens</th>
+            <th>Valor</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>Joãozin</td>
+              <td><MiniThumbsProducts /></td>
+              <td className="">
+                {new Intl.NumberFormat('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL'
+                }).format(150)}
+              </td>
+              <td>
+                {new Intl.DateTimeFormat('pt-BR').format(
+                  new Date(Date.now())
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Container>
+  );
+}
