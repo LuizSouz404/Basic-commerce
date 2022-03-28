57ba53328ff6bb8bac9da2e7cc4cea0979bb9635
LuizSouz404 <luiz.souz404@gmail.com>
2022-03-28 18:44:08 -0300

    feat: update dashboard to edit components

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -1,4 +1,6 @@
 import { NextPage } from "next";
+import { Summary } from "../../components/AdminComponents/Summary";
+import { TransactionsTable } from "../../components/AdminComponents/TransactionTable";
 import { AdminTab } from "../../components/AdminTab";
 import { Header } from "../../components/Header";
 
@@ -6,9 +8,11 @@ const Dashboard: NextPage = () => {
   return (
     <>
       <Header />
+      <AdminTab />
 
       <div>
-        <AdminTab />
+        <Summary />
+        <TransactionsTable />
       </div>
     </>
   )
